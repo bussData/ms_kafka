e60bbf94095e886599b71bbd8121b6b68911a04e
bussData <ana.reygonzales@gmail.com>
2026-07-10 00:02:34 -0500

    purga notificacion

--- notifications-service/src/main/java/com/tecsup/app/micro/notifications/shared/infrastructure/config/KafkaConfig.java
+++ notifications-service/src/main/java/com/tecsup/app/micro/notifications/shared/infrastructure/config/KafkaConfig.java
@@ -12,23 +12,8 @@ public class KafkaConfig {
 
     // Set TOPICS
     public static final String COURSE_EVENTS_TOPIC = "course.events";
-
-
-    // DLQ
-    public static final String DLQ_COURSE_EVENTS_TOPIC = "dlq.course.events";  // ✅ DLQ Topic
-
-
-    // SAGA
-    public static final String ENROLLMENT_REQUEST_TOPIC = "enrollment.requested";
     public static final String ENROLLMENT_EVENTS_TOPIC = "enrollment.events";
-    public static final String ENROLLMENT_UPDATE_TOPIC = "enrollment.update";
-    public static final String PAYMENT_PROCESSED_TOPIC = "payment.processed";   // AGREGAR
-
-    public static final String PAYMENT_FAILED_TOPIC = "payment.failed";  // AGREGAR
-
-
-    // Set QUEUES/PARTITIONS
-
+    public static final String ENROLLMENT_UPDATE_TOPIC = "enrollment.events";
     /**
      *  Topic de eventos del curso
      * @return
@@ -60,45 +45,5 @@ public class KafkaConfig {
                 .build();
     }
 
-
-
-    // DLQ
-    @Bean
-    public NewTopic dlqCourseEventsTopic() {
-        return TopicBuilder.name(DLQ_COURSE_EVENTS_TOPIC)
-                .partitions(1)
-                .replicas(1)
-                .build();
-    }
-
-    // SAGA
-    @Bean
-    public NewTopic enrollmentRequestedTopic() {
-        return TopicBuilder
-                .name(ENROLLMENT_REQUEST_TOPIC)
-                .partitions(3)
-                .replicas(1)
-                .build();
-    }
-
-    @Bean
-    public NewTopic paymentProcessedTopic() {
-        return TopicBuilder
-                .name(PAYMENT_PROCESSED_TOPIC)
-                .partitions(3)
-                .replicas(1)
-                .build();
-    }
-
-
-    // NUEVO BEAN
-    @Bean
-    public NewTopic paymentFailedTopic() {
-        return TopicBuilder
-                .name(PAYMENT_FAILED_TOPIC)
-                .partitions(3)
-                .replicas(1)
-                .build();
-    }
 
 }
